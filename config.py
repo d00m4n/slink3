3b2255c416edae161d3f40c5ece65c7372ca7d57
dr_d00m4n <github@doomans.com>
2024-09-04 13:33:00 +0200

    Added config.py for script config

---
+++ config.py
@@ -0,0 +1,9 @@
+theme="default"
+# profile destination by default is the same folder
+profilepath="."
+fixed=5
+dynamic=5
+dbpath="./db/links.db"
+upload="./upload"
+log=True
+print("Config loaded")
